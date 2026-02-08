c042c646c41c63465f869ccca457efa4ff20cee9
manu <manu.saini94@gmail.com>
2026-02-08 11:44:24 +0530

    Welcome message - mycontactsapp

---
+++ src/main/java/com/apps/mycontactsapp/MyContactsAppMain.java
@@ -0,0 +1,9 @@
+package com.apps.mycontactsapp;
+
+public class MyContactsAppMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to MyContactsApp!!!");
+    }
+
+}
